04c8ce424eaa2371058cb631bdee2b8e70ad962d
juanpablo01~ <juanpabloo442@gmail.com>
2023-08-27 20:28:30 -0500

    ejemplo de limite 2

--- interfax.py
+++ interfax.py
@@ -6,3 +6,5 @@ e = a + " " + b + " " + c + " " + d
 print(e)
 ej_int = 10 ** 1000  #el limite hasta donde la memoria lo permita
 print (ej_int)
+max_float = 1.8e308  #el valor maximo que un flotante puede representar al rededor (1.8e308)
+print (max_float)
